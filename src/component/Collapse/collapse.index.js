fc04064aa3a86c39b11ca4d6a36268a76410c37c
Aman Pratap Singh <aman.singh@kuliza.com>
2020-10-16 16:42:44 +0530

    adding react boilerplate in collapse component

---
+++ src/component/Collapse/collapse.index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './collapse.style.css';
+
+export default class Collapse extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
