108e5dc805d8709486b5ad149500d7ca8ab0e3fb
Aman Pratap Singh <aman.singh@kuliza.com>
2020-12-30 18:28:47 +0530

    created collapsible button resusable component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import "./App.css";
+import Collapse from "./component/Collapse/collapse.index";
 
 function App() {
   return (
-    <div>Collpase</div>
+    <div className="mainWrap">
+      <div className="buttonWrap">
+        <Collapse title="Single Button">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          veniam quisquam laudantium officiis. Inventore assumenda pariatur quo
+          in deserunt reprehenderit molestiae nihil ut, magnam illo velit? Quasi
+          sequi magni distinctio!
+        </Collapse>
+      </div>
+      <div className="buttonWrap buttonWrap_2">
+        <Collapse title="Collapsibe Button-1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          veniam quisquam laudantium officiis. Inventore assumenda pariatur quo
+          in deserunt reprehenderit molestiae nihil ut, magnam illo velit? Quasi
+          sequi magni distinctio!
+        </Collapse>
+        <Collapse title="Collapsible Button-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          veniam quisquam laudantium officiis. Inventore assumenda pariatur quo
+          in deserunt reprehenderit molestiae nihil ut, magnam illo velit? Quasi
+          sequi magni distinctio!
+        </Collapse>
+        <Collapse title="Collapsible Button-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          veniam quisquam laudantium officiis. Inventore assumenda pariatur quo
+          in deserunt reprehenderit molestiae nihil ut, magnam illo velit? Quasi
+          sequi magni distinctio!
+        </Collapse>
+        <Collapse title="Collapsible Button-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          veniam quisquam laudantium officiis. Inventore assumenda pariatur quo
+          in deserunt reprehenderit molestiae nihil ut, magnam illo velit? Quasi
+          sequi magni distinctio!
+        </Collapse>
+      </div>
+    </div>
   );
 }
 
--- src/component/Collapse/collapse.index.js
+++ src/component/Collapse/collapse.index.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react'
-import './collapse.style.css';
+import React, { Component } from "react";
+import "./collpase.style.scss";
 
 export default class Collapse extends Component {
+  state = {
+    isClose: true,
+  };
+  handleCollapsible = () => {
+    this.setState({ isClose: !this.state.isClose }, () =>
+      console.log("this is to handle collpasible open close", this.state)
+    );
+  };
   render() {
     return (
-      <div>
-        
+      <div className="collapsibleWrap">
+        <button
+          className={
+            this.state.isClose ? "collapse" : "collapse collapse_active"
+          }
+          onClick={this.handleCollapsible}
+        >
+          {this.props.title}
+        </button>
+        <div
+          className={this.state.isClose ? "content content_show" : "content"}
+        >
+          <p>{this.props.children}</p>
+        </div>
       </div>
-    )
+    );
   }
+  1;
 }
